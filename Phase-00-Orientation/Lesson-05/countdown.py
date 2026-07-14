e3862eeb4fe484ef3b3a5650181af2be106ae5eb
ouroboros14 <vrushabhbhabal1729@gmail.com>
2026-07-14 12:29:07 +0530

    Lesson 05: While loops and countdown program

---
+++ Phase-00-Orientation/Lesson-05/countdown.py
@@ -0,0 +1,7 @@
+initial_number = int(input("Enter a number to start the countdown: "))
+
+while initial_number >= 1:
+    print(initial_number)
+    initial_number = initial_number - 1
+
+print("Liftoff!")
